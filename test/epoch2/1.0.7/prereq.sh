8bc206897a171aaa39ace5a8c680c2529cc3ff4a
tux-box <nathan.flow@gmail.com>
2022-09-08 23:12:54 -0500

    added some notes

--- test/epoch2/1.0.7/prereq.sh
+++ test/epoch2/1.0.7/prereq.sh
@@ -8,9 +8,13 @@ wget http://www.arma2.com/downloads/update/a2oa-server-1.63.112555.tar.bz2
 tar jxvf a2oa-server-1.63.112555.tar.bz2
 
 # Get the Arma 2 Linux dedicated server?
+#https://community.bistudio.com/wiki/Arma_Dedicated_Server#Linux
 
 # Get Dayz Epoch Server files
+wget https://drive.google.com/file/d/1jDn86sfTwcRae4NZgHK76k_CaY1jOUP2/view?usp=sharing
 
 # Get Dayz Epoch Client files   https://rmdhnreza.my.id/download-from-google-drive-using-command-line-interface-cli/
+https://drive.google.com/file/d/19iCJevU008g311vsxJNR0PjYmkSv36YK/view?usp=sharing
+
 wget https://drive.google.com/file/d/19iCJevU008g311vsxJNR0PjYmkSv36YK/view?usp=sharing
 wget https://drive.google.com/open?id=19iCJevU008g311vsxJNR0PjYmkSv36YK&authuser=0
