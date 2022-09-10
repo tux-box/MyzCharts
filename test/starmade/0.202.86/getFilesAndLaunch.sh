8f23d8d1cd40acd24573e3c6e7829595a7a780a5
tux-box <nathan.flow@gmail.com>
2022-09-10 16:59:31 -0500

    added addional script

---
+++ test/starmade/0.202.86/getFilesAndLaunch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#This file grabs the instller from the web and runs it once.
+
+#Dl Jave Starmade server package from webssite
+wget https://files.star-made.org/StarMade-Starter.jar -P /starmade/
+
+#launch the installer
+java -jar /starmade/StarMade-Starter.jar -nogui
+
+#launch the server
+cd /starmade/StarMade
+java -Xms1024m -Xmx2048m -jar /starmade/StarMade/StarMade.jar -server -nogui
